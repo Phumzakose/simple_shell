cd7b38a86299e7f482899d528d078739a0492bd5
Sine <ssnngcongo@gmail.com>
2023-07-20 13:56:51 -0700

    shell environment file

---
+++ env-main.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * my_env - prints environment
+ * @info: struct address
+ * Return: Always 0, success
+ */
+int my_env(info_t *info)
+{
+	print_list_str(inf->env);
+	return (0);
+}
+
+/**
+ * get_env - gets environment variable value
+ * @info: struct address
+ * @name: environment variable name
+ * Return: environment value
+ */
+
+char *get_env(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * my_unsetenv - remove environment variable
+ * @info: struct address
+ * Return: Always 0, success
+ */
+
+int my_unsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguments. \n");
+		return (1);
+	}
+	for (i = 1, i <= info->argc; i++)
+	{
+		unset_env(info, info->argv[i]);
+	}
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: struct address
+ * Return: Always 0, success
+ */
+
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+
