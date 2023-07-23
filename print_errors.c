e840f90626b88920fc6c5a3abbd77b17fe23c728
Sine <ssnngcongo@gmail.com>
2023-07-23 08:42:21 -0700

    uploaded error message files

---
+++ print_errors.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints string
+ * @str: string to be printed
+ * Return: Void
+ */
+
+void _eputs(char *str)
+{
+	into i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(stri[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - writes character to stderr
+ * @c: character to be printed
+ * Return: 1 on success, -1 on error
+ */
+
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - write character to file descriptor
+ * @c: character to be printed
+ * @fd: file descriptor to write to
+ * Return: 1 on success, -1 on error
+ */
+
+int _putfd(char c, int fd)
+{
+	static int 1;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints input string
+ * @str: string to tbe printed
+ * @fd: file descriptor to write to
+ * Return: string to be inputted
+ */
+
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
