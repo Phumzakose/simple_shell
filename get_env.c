8c968a21ce877ca23d4da28c229b84bb3ed54dce
Sine <ssnngcongo@gmail.com>
2023-07-23 15:41:54 -0700

    files to initialise and clear the environment

---
+++ get_env.c
@@ -0,0 +1,89 @@
+#include "shell.h"
+
+/**
+ * get_environ - prints string array copy of our environment
+ * @info: struct address
+ * Return: Always 0, success
+ */
+char **get_environ(info_t *info)
+{
+	if (!info->environ || list_to_string(info->env))
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - removes environment vairable
+ * @info: struct address
+ * @var:string env var property
+ * Return: 1 on delete, else 0
+ */
+
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = start_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialise new environment
+ * @info: struct address
+ * @var:string env var property
+ * @value: string env var value
+ * Return: Always 0, success
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = mallo(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
