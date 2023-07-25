c8e60ccd351d74f5bf5b79ee90e2a183aa1a8521
Sine <ssnngcongo@gmail.com>
2023-07-25 13:26:13 -0700

    created the memory reallocation file

---
+++ realloc.c
@@ -0,0 +1,65 @@
+#include "shell.h"
+
+/**
+ * mem_set - fills memory with a constant byte
+ * @s: pointer to memory area
+ * @b: byte to fill *s
+ * @n: amount of bytes to be filled
+ * Return: pointer to memory area s
+ */
+
+char *mem_set(char *s, char b, unsigned int n)
+{
+	unsigned it i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ * Return: void
+ */
+
+void ffree(char *pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates block of memory
+ * @ptr: pointer to previously allocated block
+ * @old_size: byte size of previous block
+ * @new_size: bytes size of new block
+ * Return: pointer to old block
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new));
+	if (!new_size)
+		return (free(ptr), NULL);
+
+	if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
